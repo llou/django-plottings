bbc1a268856e60ab562302fad356143307a03c6e
Jorge Monforte González <jorge@monforte.tech>
2024-07-12 08:35:49 +0200

    fix library docstring

--- plottings/__init__.py
+++ plottings/__init__.py
@@ -1,10 +1,10 @@
-from .file import SVGPlotToFile, PNGPlotToFile
-from .views import PNGPlotView, SVGPlotView
-from .memory import SVGPlot, PNGBase64Plot
-
 """
 A library to generate Matplotilib graphics within django applications
 """
+
+from .file import SVGPlotToFile, PNGPlotToFile
+from .views import PNGPlotView, SVGPlotView
+from .memory import SVGPlot, PNGBase64Plot
 
 __all__ = ["SVGPlotToFile",
            "PNGPlotToFile",
