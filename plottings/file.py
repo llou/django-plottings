bc7185c424d9945cd7c06b8bf9738d45afecb744
Jorge Monforte González <yo@llou.net>
2024-07-26 19:27:51 +0200

    add some more docstrings

--- plottings/file.py
+++ plottings/file.py
@@ -2,8 +2,16 @@ from .base import BasePlot, FileMixin, SVGZPlotMixin, PNGPlotMixin
 
 
 class SVGZPlotToFile(SVGZPlotMixin, FileMixin, BasePlot):
+    """
+    This class is used to create a django a file object storing a SVG plot
+    of the plotter function
+    """
     pass
 
 
 class PNGPlotToFile(PNGPlotMixin, FileMixin, BasePlot):
+    """
+    This class is used to create a django a file object storing a PNG plot
+    of the plotter function
+    """
     pass
--- plottings/value.py
+++ plottings/value.py
@@ -8,9 +8,18 @@ from .base import (
 
 
 class SVGPlotToValue(SVGPlotMixin, ValueMixin, BasePlot):
+    """
+    This class is used to create a SVG figure into a Value object ready to
+    be passed to the template engine to render it in a webpage.
+    """
     pass
 
 
 class PNGBase64PlotToValue(Base64PlotMixin, PNGPlotMixin, ValueMixin,
                            BasePlot):
+    """
+    This class is used to dump a PNG figure encoded in Base64 format into a
+    Value object ready to be passed to the template engine to render it in a
+    webpage.
+    """
     pass
